25cdf2bc54e9458077316bab693551f30482f897
Sarthak22458 <133597552+Sarthak22458@users.noreply.github.com>
2023-05-17 00:12:53 +0530

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -339,3 +339,127 @@ def taking_input_in_label_dictionary(key,label_name_dictionary,data):
     
     label_name_dictionary[key]=value
 # -----------------------------------------------------Satkeerat Singh end--------------------------------------------
+# -------------------------------------------------Sarthak Srivastav start--------------------------------------
+
+# ----------------------extra function(used in satwik's function IGNORE IT) ----------------------------------------------------------------------
+def key(instruction):
+    all_isa=["add","sub","mov","ld","st","mul","div","rs","ls","xor","or","and","not","cmp","jmp","jlt","jgt","je","hlt"]
+    for i in range(1,len(instruction)):
+        if(instruction[i] not in all_isa):
+            key=i
+    return key
+
+
+
+# -------------------------------------ISA(called function)----------------
+def addition(r1, r2, r3):
+    if r1 in reg_lst and r2 in reg_lst and r3 in reg_lst:
+        return "0000000"+register(r1)+register(r2)+register(r3)
+
+def subtraction(r1, r2, r3):
+    if r1 in reg_lst and r2 in reg_lst and r3 in reg_lst:
+        return "0000100"+register(r1)+register(r2)+register(r3)
+
+def move_immediate(r, imm_val):
+    # print("                   hi                           ")
+    # print("imm value",imm_val[1:])
+    # print("imm_val binary",integer_to_binary_with_padding(imm_val[1:]))
+    
+    if r in reg_lst:
+        return "000100"+register(r)+integer_to_binary_with_padding(imm_val[1:])
+
+
+
+def move_register(r1, r2):
+    # print("              ",r2)
+    if r1 in reg_lst and r2 in reg_lst:
+        return "0001100000"+register(r1)+register(r2)
+
+def load(r,var, var_dic):
+    return "001000"+register(r)+var_dic[var]
+
+def store(r,var,var_dic):
+    return "001010"+register(r)+var_dic[var]
+
+def multiply(r1,r2,r3):
+    
+    return "0011000"+register(r1)+register(r2)+register(r3)
+
+
+
+def divide(r1,r2):
+    return "0011100000"+register(r1)+register(r2)
+
+def right_shift(r1,imm):
+    
+    return "010000"+register(r1)+integer_to_binary_with_padding(imm)
+
+
+
+def left_shift(r1,imm):
+    
+    return "010010"+register(r1)+integer_to_binary_with_padding(imm)
+
+
+
+def exclusive_OR(r1,r2,r3):
+    if r1 in reg_lst and r2 in reg_lst and r3 in reg_lst:
+        return "0101000"+register(r1)+register(r2)+register(r3)
+
+
+
+def Or(r1,r2,r3):
+    if r1 in reg_lst and r2 in reg_lst and r3 in reg_lst:
+        return "0101100"+register(r1)+register(r2)+register(r3)
+
+
+
+def And(r1,r2,r3):
+     if r1 in reg_lst and r2 in reg_lst and r3 in reg_lst:
+        return "0110000"+register(r1)+register(r2)+register(r3)
+
+
+def invert(r1,r2):
+    if r1 in reg_lst and r2 in reg_lst:
+        return "0110100000"+register(r1)+register(r2)
+
+
+def compare(r1,r2):
+    # print("             r1",r1)
+    # print("             r2",r2)
+    if r1 in reg_lst and r2 in reg_lst:
+        return "0111000000"+register(r1)+register(r2)
+
+
+def unconditional_jump(instruction,label_name_dictionary,data):
+    k=key(instruction)
+    taking_input_in_label_dictionary(instruction[k],label_name_dictionary,data)
+    return "011110000"+label_name_dictionary[instruction[k]]
+
+
+def jump_if_less_than(instruction,label_name_dictionary,data):
+    k=key(instruction)
+    taking_input_in_label_dictionary(instruction[k],label_name_dictionary,data)
+    return "111000000"+label_name_dictionary[instruction[k]]
+
+
+
+def jump_if_greater_than(instruction,label_name_dictionary,data):
+    k=key(instruction)
+    taking_input_in_label_dictionary(instruction[k],label_name_dictionary,data)
+    return "111010000"+label_name_dictionary[instruction[k]]
+
+
+
+def jump_if_equal(instruction,label_name_dictionary,data):
+    k=key(instruction)
+    taking_input_in_label_dictionary(instruction[k],label_name_dictionary,data)
+    return "111110000"+label_name_dictionary[instruction[k]]
+
+
+
+def halt():
+    return "1101000000000000"
+
+
+# -------------------------------------------------Sarthak Srivastav end---------------------------------------------
